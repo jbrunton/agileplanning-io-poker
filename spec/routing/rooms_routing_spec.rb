20b463ff8d634ee48337187bf001a09e376c4cef
John Brunton <john_brunton@hotmail.co.uk>
2015-03-28 14:19:11 +0000

    fix failing routing specs

--- spec/routing/rooms_routing_spec.rb
+++ spec/routing/rooms_routing_spec.rb
@@ -15,16 +15,8 @@ RSpec.describe RoomsController, type: :routing do
       expect(:get => "/rooms/1").to route_to("rooms#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/rooms/1/edit").to route_to("rooms#edit", :id => "1")
-    end
-
     it "routes to #create" do
       expect(:post => "/rooms").to route_to("rooms#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/rooms/1").to route_to("rooms#update", :id => "1")
     end
 
     it "routes to #destroy" do
