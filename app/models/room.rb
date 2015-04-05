533275b3da5927af76cf237494f9eeb393696516
John Brunton <john_brunton@hotmail.co.uk>
2015-04-05 11:16:26 +0100

    #13 add display_name method to Room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,7 @@
 class Room < ActiveRecord::Base
   has_many :attendees
+
+  def display_name
+    "Room ##{to_param}"
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:room) }
+
+  context "#display_name" do
+    it "returns the room name" do
+      expect(subject.display_name).to eq("Room ##{subject.to_param}")
+    end
+  end
 end
